e6b3420e600767885ceda1b48d7538748508ad0d
Ignatenko <ignsv@yahoo.com>
2016-03-24 03:44:09 +0200

    Try to do task4. Write new wrapper

--- sandboxedModule/ru/framework.js
+++ sandboxedModule/ru/framework.js
@@ -10,12 +10,24 @@ var fs = require('fs'),
 
 
 // Создаем контекст-песочницу, которая станет глобальным контекстом приложения
-var context = { module: {}, console: console, setTimeout:setTimeout, setInterval:setInterval, util:util };
+var context = { module: {}, console: clone(console), setTimeout:setTimeout, setInterval:setInterval, util:util };
 context.global = context;
 var sandbox = vm.createContext(context);
 
+
+
 // Читаем исходный код приложения из файла
 var fileName = './'+process.argv[2];
+
+context.console.log = function(msg) { 
+var date = new Date().toLocaleTimeString(); 
+console.log('['+date+'] '+fileName+' » '+msg);
+};
+function clone(obj) {
+     var res = {};
+     for (var key in obj) res[key] = obj[key];
+     return res;
+ }
 fs.readFile(fileName, function(err, src) {
   // Тут нужно обработать ошибки
   if (err){
